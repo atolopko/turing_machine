5fdfa9284a5c7c0ca7993560addff36ec1bf7f41
Andrew Tolopko <github@tolopko.com>
2016-10-09 21:07:42 -0400

    TuringMachine implementation
    - Add execution of operations
    - Work in progress

--- lib/turing_machine.ex
+++ lib/turing_machine.ex
@@ -1,2 +1,41 @@
 defmodule TuringMachine do
+  defstruct state: nil, tape: %Tape{}
+
+  def execute(%{ state: :halt, tape: tape}, program) do
+    %TuringMachine{ state: :halt, tape: tape }
+  end
+
+  def execute(tm, program) do
+    { operations, to_state } =
+      Program.find(program, tm.state, tm.tape.read)
+    tape = execute(operations, tm.tape)
+    tm = %TuringMachine{ state: to_state,
+                         tape: tape }
+    execute(tm, program)
+  end
+
+  def execute_ops([], tape) do
+    IO.inspect tape
+    tape
+  end
+
+  def execute_ops([:left | operations], tape) do
+    IO.inspect tape
+    execute_ops(operations, Tape.move_left(tape))
+  end
+
+  def execute_ops([:right | operations], tape) do
+    IO.inspect tape
+    execute_ops(operations, Tape.move_right(tape))
+  end
+  
+  def execute_ops([:erase | operations], tape) do
+    IO.inspect tape
+    execute_ops(operations, Tape.erase(tape))
+  end
+  
+  def execute_ops([{ :write, symbol } | operations], tape) do
+    IO.inspect tape
+    execute_ops(operations, Tape.write(tape, symbol))
+  end
 end
--- test/turing_machine_test.exs
+++ test/turing_machine_test.exs
@@ -2,7 +2,40 @@ defmodule TuringMachineTest do
   use ExUnit.Case
   doctest TuringMachine
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "execute write op" do
+    tape = TuringMachine.execute_ops([{ :write, 1 }], %Tape{})
+    assert Tape.read(tape) == 1
+  end
+
+  test "execute erase op" do
+    tape = Tape.write(%Tape{}, 1)
+    tape = TuringMachine.execute_ops([:erase], tape)
+    assert Tape.read(tape) == nil
+  end
+
+  test "execute left op" do
+    tape = Tape.write(%Tape{}, 1) |> Tape.move_right |> Tape.write(2)
+    tape = TuringMachine.execute_ops([:left], tape)
+    assert Tape.read(tape) == 1
+  end
+
+  test "execute right op" do
+    tape = Tape.write(%Tape{}, 1) |> Tape.move_left |> Tape.write(2)
+    tape = TuringMachine.execute_ops([:right], tape)
+    assert Tape.read(tape) == 1
+  end
+
+  test "execute ops sequence" do
+    tape =
+      TuringMachine.execute_ops(
+        [{ :write, 1 },
+         :left,
+         { :write, 2 },
+         :right],
+        %Tape{})
+    IO.inspect tape
+    assert Tape.read(tape) == 1
+    tape = Tape.move_left(tape)
+    assert Tape.read(tape) == 2
   end
 end
