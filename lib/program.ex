f244991859d70274ddb4b81da1929ffc91c42984
Andrew Tolopko <github@tolopko.com>
2016-10-10 18:37:41 -0400

    Add ProgramError

--- lib/program.ex
+++ lib/program.ex
@@ -1,5 +1,5 @@
 defmodule Program do
-  def add(prog, from_state, input, operations, to_state) do
+  def add(prog \\ %{}, from_state, input, operations, to_state) do
     x = { operations, to_state }
     Map.update(
       prog,
@@ -9,6 +9,14 @@ defmodule Program do
   end
 
   def find(prog, state, input) do
+    unless Map.has_key?(prog, state),
+      do: raise(ProgramError, "undefined state #{inspect state}") 
+    unless Map.has_key?(prog[state], input),
+      do: raise(ProgramError, "undefined input #{inspect input} for state #{inspect state}") 
     prog[state][input]
   end
+end
+
+defmodule ProgramError do
+  defexception message: "program error"
 end
--- test/program_test.exs
+++ test/program_test.exs
@@ -2,6 +2,11 @@ defmodule ProgramTest do
   use ExUnit.Case
   doctest Program
 
+  test "add initial rule" do
+    p = Program.add(:initial, nil, [:right], :s1)
+    assert Program.find(p, :initial, nil) == {[:right], :s1}
+  end
+
   test "add rules and find" do
     p = %{}
     p = Program.add(p, :s0, nil, [:right], :s1)
@@ -14,7 +19,18 @@ defmodule ProgramTest do
     assert Program.find(p, :s2, 3) == {[:erase, :right], :s3}
   end
 
-  # test "find non-extant rule" do
-  #   assert Program.find(p, :s4, 1) == nil
-  # end
+  test "execute program with ummatched program state" do
+    p = Program.add(:initial, nil, [], :halt)
+    assert_raise ProgramError, "undefined state :missing", fn ->
+      Program.find(p, :missing, nil)
+    end
+  end
+
+  test "execute program with ummatched program input" do
+    p = Program.add(:s0, 1, [:right], :halt)
+    assert_raise ProgramError, "undefined input 2 for state :s0", fn ->
+      Program.find(p, :s0, 2)
+    end
+  end
 end
+
