1d611c1f65fb54dfeaa3345a42ad6161584941dc
Andrew Tolopko <github@tolopko.com>
2016-10-11 20:56:03 -0400

    Update example program
    Use the first example program presented in Turing's paper, which
    generates an irrational number.

--- example.exs
+++ example.exs
@@ -3,11 +3,21 @@ defmodule Example do
 
   def run do
     prog =
-      add(:initial, nil, [{:write, 1}, :right], :s1) |>
-      add(:s1, nil, [{:write, 2}, :left, :left], :s2) |>
-      add(:s1, 1, [:erase, :right, {:write, 1}, :right], :s2) |>
-      add(:s2, nil, [:erase], :halt)
-    tm = TuringMachine.execute(prog)
+      add(:b, nil, [{:write, "e"}, :right,
+                    {:write, "e"}, :right,
+                    {:write, 0}, :right, :right,
+                    {:write, 0}, :left, :left], :o) |>
+      add(:o, 1, [:right, {:write, "x"}, :left, :left, :left], :o) |>
+      add(:o, 0, [], :q) |>
+      add(:q, :any_symbol, [:right, :right], :q) |>
+      add(:q, nil, [{:write, 1}, :left], :p) |>
+      add(:p, "x", [:erase, :right], :q) |>
+      add(:p, "e", [:right], :f) |>
+      add(:p, nil, [:left, :left], :p) |>
+      add(:f, :any_symbol, [:right, :right], :f) |>
+      add(:f, nil, [{:write, 0}, :left, :left], :o)
+    tm = %TuringMachine{ state: :b }
+    tm = TuringMachine.execute(tm, prog)
     IO.inspect tm.tape
   end
 end
